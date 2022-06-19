d72eae4b33ee8489a7c899b77f23d74fa9aaa78c
Koehler <ckoehler16@gmail.com>
2022-06-19 17:00:58 -0400

    having issues with .env

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: 'company_db'
 });
 
 // Start mysql server and connect to database
